2e0506220b3a74a041e05f0118112a94a0024bc0
joshchae <joshchae.de@gmail.com>
2021-05-31 23:54:43 +0200

    comment out climateApi

--- src/components/ClimateApi.js
+++ src/components/ClimateApi.js
@@ -1,21 +1,10 @@
-import { useState, useEffect } from 'react'
-// import axios from 'axios'
+// import { useState, useEffect } from 'react'
+// // import axios from 'axios'
 
-//++++++++++++FETCH BACKEND API+++++++++++++++++
-function ClimateApi() {
-  const [locationData, setLocationData] = useState([]);
+// //++++++++++++FETCH BACKEND API+++++++++++++++++
+// function ClimateApi() {
   
-    useEffect(() => {
-        fetch('http://localhost:5000/locations/')
-        .then(res => res.json())
-        .then((json) => {
-          setLocationData(json.data.data);
-          console.log(json.data)
-        })
-        .catch(console.log('request failed, useApi'));
-      }, [])
-
-      return {locationData}
-}
-export default ClimateApi
+//       return {locationData}
+// }
+// export default ClimateApi
 
