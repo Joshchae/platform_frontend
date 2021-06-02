26113ebf3ddb0ac368ef92d7c184640430b9474b
Jon Tokugawa <79482239+Joldon@users.noreply.github.com>
2021-06-02 11:37:17 +0200

    fey intervention

--- src/pages/conflict/ConflictHome2.js
+++ src/pages/conflict/ConflictHome2.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactTooltip from 'react-tooltip';
 // import {fetchConflicts} from 'useData'
 import MapChart from './MapChart'
 
 function ConflictHome2() {
     
+    const [content, setContent] = useState("");
+
     return (
-        <div>
-            <MapChart />
-        </div>
-    )
+      <div>
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
+      </div>
+    );
 }
 
 export default ConflictHome2
--- src/pages/conflict/MapChart.js
+++ src/pages/conflict/MapChart.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { csv } from "d3-fetch";
 import { scaleLinear } from "d3-scale";
 import {
   ComposableMap,
@@ -8,22 +7,65 @@ import {
   Sphere,
   Graticule
 } from "react-simple-maps";
-import vulnerability from './vulnerability.csv'
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const colorScale = scaleLinear()
-  .domain([0.29, 0.68])
-  .range(["#ffedea", "#ff5233"]);
+const group = (objectArray, property) => {
+  return objectArray.reduce((acc, obj) => {
+    const key = obj[property];
+    if (!acc[key]) {
+      return {
+        ...acc,
+        [key]: {
+          conflicts: 1,
+          fatalities: obj.best,
+          countryName: obj.country
+        }
+      };
+    }
 
-const MapChart = () => {
-  const [data, setData] = useState([]);
+    return {
+      ...acc,
+      [key]: {
+        ...acc[key],
+        conflicts: acc[key].conflicts + 1
+      }
+    };
+  }, {});
+};
+
+const MapChart = ({ setTooltipContent }) => {
+  const [data, setData] = useState({});
+  const [maxConflicts, setMaxConflicts] = useState(0);
+
+  const colorScale = scaleLinear()
+    .domain([0, maxConflicts])
+    .range(["#ffedea", "#ff5233"]);
+
+  const fetchConflicts = async () => {
+    const res = await fetch(
+      "https://ucdpapi.pcr.uu.se/api/gedevents/20.1?pagesize=1000"
+    );
+    const json = await res.json();
+    const data = json.Result.map((item) => ({
+      ...item,
+      countryCode: item.relid.substring(0, 3)
+    }));
+
+    const groupedData = group(data, "countryCode");
+    const maxConflicts = Object.keys(groupedData).reduce((max, country) =>
+      max > groupedData[country].conflicts
+        ? max
+        : groupedData[country].conflicts
+    );
+
+    setData(groupedData);
+    setMaxConflicts(maxConflicts);
+  };
 
   useEffect(() => {
-    csv(vulnerability).then((data) => {
-      setData(data);
-    });
+    fetchConflicts();
   }, []);
 
   return (
@@ -32,25 +74,37 @@ const MapChart = () => {
         rotate: [-10, 0, 0],
         scale: 147
       }}
+      data-tip=""
     >
       <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
       <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={d ? colorScale(d["2017"]) : "#F5F4F6"}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => {
+            const countryCode = geo.properties.ISO_A3;
+            const d = data[countryCode];
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={d ? colorScale(d.conflicts) : "#F5F4F6"}
+                onMouseEnter={() => {
+                  const { NAME } = geo.properties;
+
+                  try {
+                    setTooltipContent(`${NAME} — ${d.conflicts || 0}`);
+                  } catch {
+                    setTooltipContent("");
+                  }
+                }}
+                onMouseLeave={() => {
+                  setTooltipContent("");
+                }}
+              />
+            );
+          })
+        }
+      </Geographies>
     </ComposableMap>
   );
 };
