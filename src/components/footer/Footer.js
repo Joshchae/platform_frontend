eec51a8746e7dce285fbeb330494cb23ae58df5a
zuzuc <zuzu@posteo.de>
2021-06-09 12:59:23 +0200

    final fact cards, footer, about

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,16 +8,14 @@ import { CgCopyright } from 'react-icons/cg';
 function Footer() {
     const iconStyles = { color: ' #00303f', fontSize: "0.8em" };
     return (
-        <div className='footer-column'>
-            <div className='disclaimer-copyright'>
-                <div className='disclaimer'>
-                    <Disclaimer />Disclaimer
-                </div>
-                <div className='copyright'>
-                    <p><CgCopyright style={iconStyles} /> 2021 by Josh, Franzi, Joldon </p>
-                </div>
+        <div className='disclaimer-copyright'>
+            <div className='disclaimer'>
+                <Disclaimer />Disclaimer
             </div>
-        </div>          
+            <div className='copyright'>
+                <p><CgCopyright style={iconStyles} /> 2021 by Josh, Franzi, Joldon </p>
+            </div>
+        </div>    
     )
 }
 
