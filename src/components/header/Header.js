ae41a40cb2e345dd17ecd217115746f6e78bf88c
Josh Chae <75668069+Joshchae@users.noreply.github.com>
2021-06-10 19:13:56 +0200

    retrieve NavItem from React Bootstrap

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import {Link} from 'react-router-dom'
 // import '../../App.css';
 import './Header.css';
 import "../../scss/custom.scss";
@@ -12,36 +11,24 @@ function Header() {
         <div className='header'>
             <Navbar sticky='top' expand='lg' variant='primary' bg='success' >
             <Navbar.Brand  /> 
-            <Link to="/"><img src={logo} width='60vw' height='60vh' alt="mapstories" /></Link>
+            <img src={logo} width='60vw' height='60vh' alt="mapstories" />
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                <Nav.Link to="/">
-                    <Link to="/">Home</Link>
-                </Nav.Link>
-                <Nav.Link to="/about">
-                    <Link to="/about">About</Link>
-                </Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
                 {/* <Nav.Link href="/ClimateMigration">Climate-induced Migration</Nav.Link>
                 <Nav.Link href="/Conflicts">Conflicts</Nav.Link> */}
                 <NavDropdown title="Climate-induced Migration" id="basic-nav-dropdown">
                 {/* <NavDropdown.Item href="/ClimateMigration">Start</NavDropdown.Item> */}
-                <NavDropdown.Item>
-                    <Link to="/ClimateMigration/Facts">Facts</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                    <Link to="/ClimateMigration/Map">Map</Link>
-                </NavDropdown.Item>
+                <NavDropdown.Item href="/ClimateMigration/Facts">Facts</NavDropdown.Item>
+                <NavDropdown.Item href="/ClimateMigration/Map">Map</NavDropdown.Item>
                 {/* <NavDropdown.Item href="/ClimateMigration/Stories">Stories</NavDropdown.Item> */}
                 <NavDropdown.Divider />
-                <NavDropdown.Item>
-                    <Link to="/ClimateMigration/Submit">Submit your story</Link>
-                </NavDropdown.Item>
+                <NavDropdown.Item href="/ClimateMigration/Submit">Submit your story</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Conflicts" id="basic-nav-dropdown">
-                <NavDropdown.Item>
-                    <Link to="/Conflicts">Map</Link>
-                </NavDropdown.Item>
+                    <NavDropdown.Item href="/Conflicts">Map</NavDropdown.Item>
                     {/* <NavDropdown.Item href="/Conflicts2">Map 2</NavDropdown.Item> */}
                 </NavDropdown>
                 </Nav>
